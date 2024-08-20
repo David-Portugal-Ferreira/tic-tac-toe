b8e6abc1c9608fa17b09b7f203091db69f573b86
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-08-20 16:26:26 +0100

    Add layout and layout style. Make player change auto

--- functions.js
+++ functions.js
@@ -10,30 +10,30 @@ let gameBoard = (function () {
 
     const incrementTurn = () => ++turn;
 
-    const setPosition = (index, type) => {
-        gameboard[index] = type;
+    const setPosition = (index) => {
+        gameboard[index] = player;
         checkGameStatus();
         incrementTurn()
         setPlayer();
     }
-    const getPositions = () => gameboard.length;
+    const getPosition = () => gameboard;
 
     const checkGameStatus = () => {
-        for(let i = 0; i < 2; i++) {
+        for (let i = 0; i < 2; i++) {
             let type = '';
             i === 0 ? type = 'X' : type = 'Y';
             // TODO - think of other way to do this 
-            if(
-                (gameboard[0] === type && gameboard[1] === type && gameboard[2] === type) ||
-                (gameboard[3] === type && gameboard[4] === type && gameboard[5] === type) ||
-                (gameboard[6] === type && gameboard[7] === type && gameboard[8] === type) ||
-                
-                (gameboard[0] === type && gameboard[3] === type && gameboard[6] === type) ||
-                (gameboard[1] === type && gameboard[4] === type && gameboard[7] === type) ||
+            if (
+                (gameboard[1] === type && gameboard[2] === type && gameboard[3] === type) ||
+                (gameboard[4] === type && gameboard[3] === type && gameboard[4] === type) ||
+                (gameboard[7] === type && gameboard[4] === type && gameboard[9] === type) ||
+
+                (gameboard[1] === type && gameboard[4] === type && gameboard[8] === type) ||
                 (gameboard[2] === type && gameboard[5] === type && gameboard[8] === type) ||
-    
-                (gameboard[0] === type && gameboard[4] === type && gameboard[8] === type) ||
-                (gameboard[2] === type && gameboard[4] === type && gameboard[6] === type)
+                (gameboard[3] === type && gameboard[6] === type && gameboard[9] === type) ||
+
+                (gameboard[1] === type && gameboard[5] === type && gameboard[9] === type) ||
+                (gameboard[3] === type && gameboard[5] === type && gameboard[7] === type)
             ) {
                 alert(type + ' WINS');
             }
@@ -45,11 +45,17 @@ let gameBoard = (function () {
         turn = 1;
     };
 
-    return { setPosition, getPositions, getPlayer}
+    return { setPosition, getPosition }
 })();
 
 let displayController = (function () {
-    
+
+    const renderBoard = () => {
+
+        
+    }
+
+    return { renderBoard }
 })();
 
 function createUser(name) {
